b4aa06fed43ab9bb37b9b32113253dd599c98067
Nguyen Quoc Tham <tham.nguyenquoc@gameloft.com>
2019-10-08 10:36:25 +0700

    - base ts compile to js - copy index.html file

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    port: 8080
+  },
+  plugins: [
+    new CopyPlugin([
+      { from: 'src/index.html', to: 'index.html' },
+    ]),
+  ],
+};
